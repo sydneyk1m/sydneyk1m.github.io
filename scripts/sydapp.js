b6ffd4dcc79be09bd52f4b66506883a68dd89f9c
Sydney Kim <snk5@rice.edu>
2024-06-29 21:23:03 -0400

    fixed faulty js import link

--- scripts/sydapp.js
+++ scripts/sydapp.js
@@ -1,1 +1,18 @@
 
+function on() {
+    // displays overlay
+    const turnOn = document.getElementById("overlay");
+    turnOn.style.display = "block";
+    // turn off vertical scrolls when overlay is displayed
+    const overflow = document.querySelector("body");
+    overflow.style.overflow = "hidden";
+}
+
+function off() {
+    // displays overlay
+    const turnOff = document.getElementById("overlay");
+    turnOff.style.display = "none";
+    // turn off vertical scrolls when overlay is displayed
+    const overflow = document.querySelector("body");
+    overflow.style.overflow = "";
+}
